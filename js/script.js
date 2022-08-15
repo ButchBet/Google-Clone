22e843bedc321ea73747320ef281d4cdcb796e74
ButchBet <butchbet01@gmail.com>
2022-08-15 16:35:50 -0500

    Added more resonsive features.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 import "../components/product.js"
 
 // Script to set the corret of the height of the space in order to make the wiew of the page better
-
-function setHeight() {
-}
-
 setInterval(() => {
-    console.log("Resized")
     const bodyHeight = document.body.offsetHeight;
     const container = document.getElementById("container");
     const space = document.getElementById("space");
@@ -14,17 +9,10 @@ setInterval(() => {
     
     var height = bodyHeight - containerHeight;    
     if(height > 0 || height < 0) {
-        console.log(height);
         height += space.scrollHeight;
 
         const newHeight = `height: ${height}px`;
 
-        console.log(newHeight);
-
         document.getElementById("space").setAttribute("style", newHeight);
-    } else {
-        console.log("The height is correct.")
-    }
+    } 
 }, 500);
-
-// window.addEventListener("resize", setHeight());
